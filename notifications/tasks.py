238766dcb14e7b7b884b3b7b9928ef9d9ff746a8
kosumosu <kosumosu7air@gmail.com>
2022-02-28 00:49:37 +0300

    поправил пару ошибок, немного раскидал логгеров, но без настройки LOGGING в settings

--- notifications/tasks.py
+++ notifications/tasks.py
@@ -7,9 +7,14 @@ import requests
 from django.utils import timezone
 from django.template.loader import render_to_string
 from django.core.mail import mail_admins
+import logging
 
 
-def send_mails(mailing):
+# Создаём логгер
+logger = logging.getLogger(__name__)
+
+
+def send_mailing(mailing):
     """
     Отправка рассылки
 
@@ -26,6 +31,8 @@ def send_mails(mailing):
     # если сообщения уже есть, то запускалась,
     # надо попробовать переслать те, что не получилось
     if mailing.messages.exists():
+        logger.info(f'Повторная отправка рассылки {mailing.id}...')
+
         finished = all([send_client(message.client, mailing)
                         for message in mailing.messages.all()
                         if message.sent is False])
@@ -33,6 +40,8 @@ def send_mails(mailing):
     # если сообщений нет, то рассылка ещё не запускалась,
     # формируем её
     else:
+        logger.info(f'Создание рассылки {mailing.id}...')
+
         # достаём фильтры для рассылки
         tags = mailing.tags.all()
         operators = mailing.operators.all()
@@ -41,14 +50,22 @@ def send_mails(mailing):
         # получаем кверисет клиентов для рассылки
         clients = Client.objects.filter(Q(tag__in=tags) | Q(operator__in=operators))
 
+        logger.debug(f'Клиенты на отправку: {clients}')
+
         # перебор клиентов, запуск рассылки по ним и возврат ответов:
         # если все True, то рассылка завершена и её можно закрывать
         finished = all([send_client(client, mailing) for client in clients])
 
-    # если всё отправилось, то отмечаем рассылку, как завершённый
-    mailing.finished = True if finished else False
-    mailing.finished_datetime = datetime.now(tz=timezone.utc)
-    mailing.save()
+    # если всё отправилось, то отмечаем рассылку, как завершённая
+    if finished:
+        mailing.finished = True
+        mailing.finished_datetime = datetime.now(tz=timezone.utc)
+        mailing.save()
+
+        logger.info(f'Рассылка {mailing.id} завершена {mailing.finished_datetime}')
+
+    else:
+        logger.info(f'Рассылка {mailing.id} не завершена')
 
 
 # функция рассылки сообщения конкретному клиенту
@@ -80,6 +97,7 @@ def send_client(client, mailing):
 
     if created:
         message.save()
+        logger.info(f'Сообщение {message.id} создано')
 
     # подготовка и отправка рассылки клиенту
     # адрес внешнего сервиса отправки
@@ -105,13 +123,13 @@ def send_client(client, mailing):
         response = requests.post(url=url + str(message.id), data=data, headers=headers, timeout=5)
 
         # DEBUG
-        print(response)
+        logger.debug(response)
 
         response.raise_for_status()
 
     # исключения можно конкретизировать, если нужно
     except Exception as e:
-        print(f'Ошибка: {e}')
+        logger.warning(f'Ошибка: {e}')
         return False
     else:
         if response.status_code == requests.codes.ok:
@@ -119,6 +137,7 @@ def send_client(client, mailing):
             message.sent_datetime = datetime.now(tz=timezone.utc)
             message.sent = True
             message.save()
+            logger.info(f'Сообщение {message.id} отправлено')
             return True
 
 
@@ -137,7 +156,7 @@ def check_and_send():
     # проходим по ним и запускаем на отправку, если они не просрочены
     for mailing in mailings_to_send:
         if mailing.stop_datetime > datetime.now(tz=timezone.utc) and not mailing.expired:
-            send_mails(mailing)
+            send_mailing(mailing)
 
         # если просрочены - помечаем, чтобы больше не трогать
         elif mailing.stop_datetime <= datetime.now(tz=timezone.utc) and not mailing.expired:
@@ -151,12 +170,14 @@ def send_starting_mailings():
     """
     раз в сутки отправляет статистику по обработанным рассылкам на email
     """
+    logger.info('Собираем рассылки за сутки')
 
     # собираем стартовавшие рассылки за сутки
     last_mailings = Mailing.objects.filter(start_datetime__gte=datetime.now(tz=timezone.utc) - timedelta(days=1))
+    logger.debug(f'Собранные рассылки: {last_mailings}')
 
-    subject = 'За прошедшие сутки было отправлено несколько рассылок'
-    text_message = 'За прошедшие сутки было отправлено несколько рассылок'
+    subject = 'За прошедшие сутки было запущено несколько рассылок'
+    text_message = 'За прошедшие сутки было запущено несколько рассылок'
 
     # рендерим в строку шаблон письма и передаём туда переменные, которые в нём используем
     render_html_template = render_to_string('send_mailings.html',
@@ -164,5 +185,6 @@ def send_starting_mailings():
                                              'subject': subject,
                                              'text_message': text_message})
 
-    # формируем письмо
+    # формируем и отправляем письмо
     mail_admins(subject=subject, message=text_message, html_message=render_html_template)
+    logger.info('Отправляем рассылки')
